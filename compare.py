a811d3e7c45d7addfeb3f951f7d64fd239d681da
Klepov Dima <d.klepov@icedigital.ru>
2016-01-21 15:57:31 +0300

    update

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
 class compare():
 
-    def compare_groups(self,people,group):
+    def compare_groups(group_him,my_group):
         pass
--- filter.py
+++ filter.py
@@ -2,16 +2,20 @@ import vk_api
 
 from main import Model
 from parse import parse
+from compare import compare
 
-test = Model(login='dima1da@yandex.ru',passw='rekord777888')
+test = Model(login='dima1da@yandex.ru', passw='rekord777888')
 parser = parse()
+compare = compare()
 
 my_group_response = test.get_subscribe(11853247)
 
+peolpe_response = test.get_local_people(483, 17, 19, 1, 6)
 
-response = test.get_local_people(483,17,19,1,6)
-
-people = parse.parse_respone_people_id(response)
+people = parse.parse_respone_people_id(peolpe_response)
 my_group = parse.parse_group(my_group_response)
 
-print(my_group)
+for i in range(len(people)):
+    temp_people = people[i]
+    group_him = parse.parse_group(test.get_subscribe(temp_people))
+    compare.compare_groups(group_him, my_group)
